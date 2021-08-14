e123a9ebbe43fe5e1613103bc71facbd986c932e
awoydeve <awoydev@gmail.comm>
2021-08-14 12:15:35 +0200

    update

--- gradient.js
+++ gradient.js
@@ -1,2 +1,4 @@
 const gradient = require('gradient-string');
-console.log(gradient.fruit("slt cv"))
+console.log(gradient.fruit("Hello World"))
+
+// List of colors : cristal, teen, mind, morning, vice, passion, fruit, instagram, atlas, retro, summer, pastel, rainbow
