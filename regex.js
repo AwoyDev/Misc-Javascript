3481eb705968acae220083d6b28f4ae862d3fabd
awoydeve <awoydev@gmail.comm>
2021-08-22 23:57:59 +0200

    regex

---
+++ regex.js
@@ -0,0 +1,3 @@
+const message = "oliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveoliveolive"
+const olive = message.match(/olive/g)
+console.log(olive.length)
