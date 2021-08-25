84ff9ab920e370d3f915202030f482b4167e582f
awoydeve <awoydev@gmail.comm>
2021-08-25 21:33:10 +0200

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const ps = require('./modules/node_modules/prompt-sync');
 const prompt = ps();
 
-  let command = prompt("> ")
-  let evaled;
-  try {
-    evaled = eval(command);
-    eval(evaled)
+let command = prompt("> ")
+let evaled;
+try {
+  evaled = eval(command);
+  eval(evaled)
 } catch (error) {
   console.log('Erreur' + error);
 }
